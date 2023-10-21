a06411bb02940e1d960299daf351e54627aaddd5
gjus <gkjuszczyk@gmail.com>
2023-10-21 14:47:23 +0200

    fix

--- ary.c
+++ ary.c
@@ -3,16 +3,6 @@
 #include <stdio.h>
 #include <assert.h>
 #include "ary.h"
-// na dzielenia trzeba sprawdzic czy min/max nie sa zero, jesli tak to ustawic na dokladnie 0.0
-// jesli mianownik to dokladnie 0 to zwrocic 0 chyba ze b tez zero
-
-// szukajac min/max do mnozenia potrzeba wziac cos niezerowego
-
-// jak dzielimy przez cos to jesli licznik jest niezerowy to jesli mianownik nie zawiera i dodatnich i ujemnych to latwo; 
-// co jak mnozymy/dzielimy przez antyprzedzial 
-
-// wynik dodawania to po prostu bedzie max wart a / min wart b itd chyba ze ktoras zero
-// dzielac potrzebuje najmniejsza dodatnia, najwieksza ujemna poza naj/najm dod/uj
 
 double inf = HUGE_VAL;
 double neg_inf = -HUGE_VAL;
---
+++ stare.md
@@ -0,0 +1,10 @@
+ na dzielenia trzeba sprawdzic czy min/max nie sa zero, jesli tak to ustawic na dokladnie 0.0
+ jesli mianownik to dokladnie 0 to zwrocic 0 chyba ze b tez zero
+
+ szukajac min/max do mnozenia potrzeba wziac cos niezerowego
+
+ jak dzielimy przez cos to jesli licznik jest niezerowy to jesli mianownik nie zawiera i dodatnich i ujemnych to latwo; 
+ co jak mnozymy/dzielimy przez antyprzedzial 
+
+ wynik dodawania to po prostu bedzie max wart a / min wart b itd chyba ze ktoras zero
+ dzielac potrzebuje najmniejsza dodatnia, najwieksza ujemna poza naj/najm dod/uj
